029fe35a5a0fa33b479434bba2f85b87a4311350
Arif Wahyu <arifwahyu696@gmail.com>
2026-02-08 18:09:07 +0700

    add functions

---
+++ src/utils/fn.ts
@@ -0,0 +1,32 @@
+"use client";
+
+import { Project, projects } from "@/constants/projects";
+import { useLayoutEffect, useState } from "react";
+
+export const useElementWidth = <T extends HTMLElement>(
+  ref: React.RefObject<T | null>,
+): number => {
+  const [width, setWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    const updateWidth = () => {
+      if (ref.current) {
+        setWidth(ref.current.offsetWidth);
+      }
+    };
+
+    updateWidth();
+    window.addEventListener("resize", updateWidth);
+    return () => window.removeEventListener("resize", updateWidth);
+  }, [ref]);
+
+  return width;
+};
+
+export const projectBySlug = projects.reduce<Record<string, Project>>(
+  (acc, project) => {
+    acc[project.slug] = project;
+    return acc;
+  },
+  {},
+);
