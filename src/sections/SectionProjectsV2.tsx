2a261387ddb17aa82df6001e7da21ed9a776b696
Arif Wahyu <arifwahyu696@gmail.com>
2026-02-08 22:21:33 +0700

    remove relative position

--- src/sections/SectionProjectsV2.tsx
+++ src/sections/SectionProjectsV2.tsx
@@ -1,11 +1,9 @@
-"use client";
-
 import { ProjectItem } from "@/components/sections/project/ProjectItem";
 import { Project, projects } from "@/constants/projects";
 
 export const SectionProjectsV2 = () => {
   return (
-    <section id="projects" className="w-full relative scroll-mt-18">
+    <section id="projects" className="w-full scroll-mt-18">
       {projects.map((item: Project, index: number) => (
         <ProjectItem key={item.id} data={item} index={index} />
       ))}
